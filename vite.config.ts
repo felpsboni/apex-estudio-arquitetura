45fee4f6756679a9e435efe0a019e6e88000b621
Felps Boni <felpsboni@users.noreply.github.com>
2026-09-01 06:10:15 -0300

    Configura GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const repoName = 'apex-estudio-arquitetura';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isProduction ? '/apex-estudio-arquitetura/' : '/',
+  base: isProduction ? `/${repoName}/` : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
